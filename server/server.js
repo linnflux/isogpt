52b95e9caa1b90e5f12f5fdec5f7b947b5af80d1
linnflux <nick@linnflux.com>
2023-02-25 01:14:10 -0500

    getting despera5

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ console.log('..........................')
 
 dotenv.config()
 
-//const { Configuration, OpenAIApi  } = require("openai");
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
